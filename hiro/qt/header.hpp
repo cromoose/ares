66d013f657d6fd076fd31af49325afdd522e6415
Moonchild <elronnd@elronnd.net>
2020-10-02 12:48:46 -0700

    hiro: get rid of qt4
    get rid of (and gitignore) autogenerated qt.moc

--- hiro/qt/header.hpp
+++ hiro/qt/header.hpp
@@ -13,11 +13,3 @@
 #undef XK_MISCELLANY
 #undef XK_LATIN1
 #include <nall/xorg/guard.hpp>
-
-//Qt 4.8.0 and earlier improperly define the QLOCATION macro
-//in C++11, it is detected as a malformed user-defined literal
-//below is a workaround to fix compilation errors caused by this
-#if HIRO_QT==4
-  #undef  QLOCATION
-  #define QLOCATION "\0" __FILE__ ":" QTOSTRING(__LINE__)
-#endif
